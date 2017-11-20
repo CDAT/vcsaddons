8bd25aa66fa39b01c5d3db6c8eb55d7c82b5a68a
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-20 11:47:33 -0800

    vcs options... not for this...

--- ci-support/circleci_mac.sh
+++ ci-support/circleci_mac.sh
@@ -2,11 +2,11 @@ export UVCDAT_ANONYMOUS_LOG=False
 export PATH=${HOME}/miniconda/bin:${PATH}
 #export VCS_BACKGROUND=0  # circleci seg faults on bg=1
 source activate py2
-python run_tests.py -v2 -n 2 -g --no-vtk-ui
+python run_tests.py -v2 -n 2
 RESULT=$?
 echo "py2 test command exit result:",$RESULT
 source activate py3
-python run_tests.py -n 2 -g --no-vtk-ui
+python run_tests.py -n 2
 RESULT=$RESULT + $?
 echo "test command exit result:",$RESULT
 if [ $RESULT -eq 0 -a $CIRCLE_BRANCH == "master" ]; then conda install -n root conda-build anaconda-client ; fi
