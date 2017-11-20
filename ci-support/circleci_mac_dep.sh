71350162fb568e6c6481d8f2393ecdfca3668489
Charles Doutriaux <doutriaux1@llnl.gov>
2017-11-20 11:41:48 -0800

    py3c files, no old-and-unmanageable circleci

--- ci-support/circleci_mac_dep.sh
+++ ci-support/circleci_mac_dep.sh
@@ -6,6 +6,7 @@ conda create -q -n py3 -c uvcdat/label/nightly -c nadeau1  -c conda-forge -c uvc
 conda create -q -n py2 -c uvcdat/label/nightly -c conda-forge -c uvcdat "cdms2>2.12.2017" nose flake8 mesalib image-compare "matplotlib<2.1" numpy=1.13 "vcs>2.12.2017"
 export UVCDAT_ANONYMOUS_LOG=False
 source activate py2
-python setup.py install --old-and-unmanageable
+python setup.py install
 source activate py3
-python setup.py install --old-and-unmanageable
+rm -rf build
+python setup.py install
