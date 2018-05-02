29a059a852652b4b62d82f1d644d48d321c713e3
Charles Doutriaux <doutriaux1@llnl.gov>
2018-05-02 11:05:25 -0700

    use cdat channel rather than uvcdat
    no need for nesii channel any more.

--- ci-support/conda_upload.sh
+++ ci-support/conda_upload.sh
@@ -2,7 +2,6 @@
 PKG_NAME=vcsaddons
 USER=cdat
 VERSION="2.12"
-ESMF_CHANNEL="nesii/label/dev-esmf"
 echo "Trying to upload conda"
 if [ `uname` == "Linux" ]; then
     OS=linux-64
@@ -30,8 +29,7 @@ export CONDA_BLD_PATH=${HOME}/conda-bld
 echo "Cloning recipes"
 git clone git://github.com/UV-CDAT/conda-recipes
 cd conda-recipes
-# uvcdat creates issues for build -c uvcdat confises package and channel
 python ./prep_for_build.py
-conda build $PKG_NAME -c cdat/label/nightly -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat 
-conda build $PKG_NAME -c cdat/label/nightly -c ${ESMF_CHANNEL} -c conda-forge -c uvcdat --python=3.6
+conda build $PKG_NAME -c cdat/label/nightly -c conda-forge -c uvcdat --python=2.7
+conda build $PKG_NAME -c cdat/label/nightly -c conda-forge -c uvcdat --python=3.6
 anaconda -t $CONDA_UPLOAD_TOKEN upload -u $USER -l nightly $CONDA_BLD_PATH/$OS/$PKG_NAME-$VERSION.`date +%Y*`0.tar.bz2 --force
