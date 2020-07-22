8c2f325d57474a814927d60f7e67774b23a46671
muryanto1 <muryanto1@llnl.gov>
2020-07-22 11:00:21 -0700

    set Version in setup.py

--- setup.py
+++ setup.py
@@ -3,25 +3,16 @@ from subprocess import Popen, PIPE
 
 src = ['Src/shpopen.c','Src/readshape.c','Src/dbfopen.c']
 
-p = Popen(
-    ("git",
-     "describe",
-     "--tags"),
-    stdin=PIPE,
-    stdout=PIPE,
-    stderr=PIPE)
 try:
-    descr = p.stdout.readlines()[0].strip().decode("utf-8")
-    print("DEBUG...descr: {d}".format(d=descr))
-    Version = "-".join(descr.split("-")[:-2])
-    print("DEBUG...Version: {v}".format(v=Version))
-    if Version == "":
-        Version = descr
-except:
-    descr = Version
+    import cdat_info
+
+    Version = cdat_info.Version
+except Exception:
+    Version = "???"
+print("VERSION:", Version)
 
 setup (name = "vcsaddons",
-       version=descr,
+       version=Version,
        description = "addons for VCS",
        url = "http://cdat.llnl.gov",
        packages = ['vcsaddons','EzTemplate','EzPlot'],
